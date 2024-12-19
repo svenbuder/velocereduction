fixed issue with overscan rms being wrong for exposures with too much flux leaking into the overscan region. using percentiles instead of standard deviation to estimate read noise. Further adjusted the FITS columns for Science, Noise, ThXe, and LC to already implement the flatfield correction (instead of reporting the data that was not yet flatfield corrected.

VeloceReduction/extraction.py
@@ -1,12 +1,26 @@
 import numpy as np
+import matplotlib.pyplot as plt
+
 from . import config
 from .utils import read_veloce_fits_image_and_metadata, match_month_to_date, polynomial_function
 
-def substract_overscan(full_image,metadata):
+def substract_overscan(full_image, metadata, debug_overscan = False):
 
     # Identify overscan region and subtract overscan while reporting median overscan and overscan root-mean-square
     overscan_median = dict()
     overscan_rms = dict()
+    
+    if debug_overscan:
+        plt.figure()
+        plt.hist(full_image.flatten(),bins=np.linspace(975,1025,100))
+        plt.show()
+        plt.close()
+
+        plt.figure(figsize=(10,10))
+        s = plt.imshow(full_image, vmin=975, vmax = 1025)
+        plt.colorbar(s)
+        plt.show()
+        plt.close()
 
     if metadata['READOUT'] == '4Amp':
 
@@ -19,8 +33,11 @@ def substract_overscan(full_image,metadata):
         overscan[:2120,2112:2112+32] = True
         overscan = full_image[overscan]
 
+        # We report the median overscan
         overscan_median['q1'] = int(np.median(overscan))
-        overscan_rms['q1'] = np.std(overscan - np.median(overscan))
+        # And we calculate a robust standard deviation, i.e.,
+        # half the difference between 16th and 84th percentile
+        overscan_rms['q1'] = np.diff(np.percentile(overscan,q=[16,84]))/2
         quadrant1 -= overscan_median['q1']
 
         # Quadrant 2: 2120: and :2112
@@ -33,7 +50,7 @@ def substract_overscan(full_image,metadata):
         overscan = full_image[overscan]
 
         overscan_median['q2'] = int(np.median(overscan))
-        overscan_rms['q2'] = np.std(overscan - np.median(overscan))
+        overscan_rms['q2'] = np.diff(np.percentile(overscan,q=[16,84]))/2
         quadrant2 = (quadrant2 - overscan_median['q2'])
 
         # Quadrant 3: 2120: and 2112:
@@ -46,7 +63,7 @@ def substract_overscan(full_image,metadata):
         overscan = full_image[overscan]
 
         overscan_median['q3'] = int(np.median(overscan))
-        overscan_rms['q3'] = np.std(overscan - np.median(overscan))
+        overscan_rms['q3'] = np.diff(np.percentile(overscan,q=[16,84]))/2
         quadrant3 -= overscan_median['q3']
 
         # Quadrant 4: :2120 and 2112:
@@ -59,7 +76,7 @@ def substract_overscan(full_image,metadata):
         overscan = full_image[overscan]
 
         overscan_median['q4'] = int(np.median(overscan))
-        overscan_rms['q4'] = np.std(overscan - np.median(overscan))
+        overscan_rms['q4'] = np.diff(np.percentile(overscan,q=[16,84]))/2
         quadrant4 -= overscan_median['q4']
 
         trimmed_image = np.hstack([np.vstack([quadrant1,quadrant2]),np.vstack([quadrant4,quadrant3])]).clip(min=0)
@@ -75,7 +92,7 @@ def substract_overscan(full_image,metadata):
         overscan[:,2112-32:2112] = True # right
         overscan = full_image[overscan]
         overscan_median['q1'] = int(np.median(overscan))
-        overscan_rms['q1'] = np.std(overscan - np.median(overscan))
+        overscan_rms['q1'] = np.diff(np.percentile(overscan,q=[16,84]))/2
         quadrant1 -= overscan_median['q1']
 
         # Quadrant 2: 2088: and :2112
@@ -87,23 +104,20 @@ def substract_overscan(full_image,metadata):
         overscan[:,-32:] = True # left        
         overscan = full_image[overscan]
         overscan_median['q2'] = int(np.median(overscan))
-        overscan_rms['q2'] = np.std(overscan - np.median(overscan))
+        overscan_rms['q2'] = np.diff(np.percentile(overscan,q=[16,84]))/2
         quadrant2 = (quadrant2 - overscan_median['q2'])
 
         trimmed_image = np.hstack([quadrant1,quadrant2]).clip(min=0)
 
-    # if config.debug:
-    #     plt.figure(figsize=(10,10))
-    #     s = plt.imshow(full_image,vmax=2200)
-    #     plt.colorbar(s)
-    #     plt.show()
-    #     plt.close()
-
-    #     plt.figure(figsize=(10,10))
-    #     s = plt.imshow(trimmed_image,vmax=1200)
-    #     plt.colorbar(s)
-    #     plt.show()
-    #     plt.close()
+    if debug_overscan:
+        plt.figure(figsize=(10,10))
+        s = plt.imshow(trimmed_image, vmin = -5, vmax = 100)
+        plt.colorbar(s)
+        plt.show()
+        plt.close()
+        
+    if debug_overscan:
+        print(overscan_median, overscan_rms, metadata['READOUT'])
 
     return(trimmed_image, overscan_median, overscan_rms, metadata['READOUT'])
 
@@ -161,7 +175,7 @@ def extract_initial_order_ranges_and_coeffs():
 
     return(initial_order_ranges, initial_order_coeffs)
 
-def extract_orders(ccd1_runs, ccd2_runs, ccd3_runs, Flat = False, LC = False, Science = False, tramline_debug = False):
+def extract_orders(ccd1_runs, ccd2_runs, ccd3_runs, Flat = False, LC = False, Science = False, debug_tramlines = False, debug_overscan=False):
     
     # Extract initial order ranges and coefficients
     initial_order_ranges, initial_order_coeffs = extract_initial_order_ranges_and_coeffs()
@@ -180,7 +194,7 @@ def extract_orders(ccd1_runs, ccd2_runs, ccd3_runs, Flat = False, LC = False, Sc
         
         for run in runs:
             full_image, metadata = read_veloce_fits_image_and_metadata(config.working_directory+'observations/'+config.date+'/ccd_'+str(ccd)+'/'+config.date[-2:]+match_month_to_date(config.date)+str(ccd)+run+'.fits')
-            trimmed_image, os_median, os_rms, readout_mode = substract_overscan(full_image, metadata)
+            trimmed_image, os_median, os_rms, readout_mode = substract_overscan(full_image, metadata, debug_overscan)
             images['ccd_'+str(ccd)].append(trimmed_image)
         
         # For science: sum counts
@@ -201,7 +215,7 @@ def extract_orders(ccd1_runs, ccd2_runs, ccd3_runs, Flat = False, LC = False, Sc
     counts_in_orders = []
     noise_in_orders = []
     
-    if tramline_debug:
+    if debug_tramlines:
         plt.figure(figsize=(15,15))
         s = plt.imshow(images['ccd_2'], vmin = 1, vmax = 20, cmap='Greys',aspect=5)
         plt.colorbar(s)
@@ -222,7 +236,7 @@ def extract_orders(ccd1_runs, ccd2_runs, ccd3_runs, Flat = False, LC = False, Sc
         order_xrange_begin = np.array(polynomial_function(np.arange(np.shape(images['ccd_'+str(ccd)])[0]),*initial_order_coeffs[order])+left,dtype=int)
         order_xrange_end   = np.array(polynomial_function(np.arange(np.shape(images['ccd_'+str(ccd)])[0]),*initial_order_coeffs[order])+right,dtype=int)
 
-        if tramline_debug:
+        if debug_tramlines:
             if ccd == '2':
                 plt.plot(order_xrange_begin,np.arange(len(order_xrange_begin)),c='C0',lw=0.5)
                 plt.plot(order_xrange_end,np.arange(len(order_xrange_begin)),c='C1',lw=0.5)
@@ -253,7 +267,7 @@ def extract_orders(ccd1_runs, ccd2_runs, ccd3_runs, Flat = False, LC = False, Sc
             if Science:
                 total_read_noise *= np.sqrt(len(runs))
 
-            if tramline_debug & Science & (x_index == 2000):
+            if debug_tramlines & Science & (x_index == 2000):
                 print(order)
                 print('x_index:      ',2000)
                 print('sum(counts):  ',np.sum(counts_in_pixels_to_be_summed,axis=0))
@@ -272,7 +286,7 @@ def extract_orders(ccd1_runs, ccd2_runs, ccd3_runs, Flat = False, LC = False, Sc
         counts_in_orders.append(order_counts)
         noise_in_orders.append(order_noise)
 
-    if tramline_debug:
+    if debug_tramlines:
         plt.xlim(2500,4200)
         plt.ylim(2000,2500)
         plt.show()

VeloceReduction_tutorial.py
@@ -22,6 +22,8 @@ from pathlib import Path
 import sys
 import argparse
 
+import matplotlib.pyplot as plt
+
 # VeloceReduction modules and function
 from VeloceReduction import config
 from VeloceReduction.utils import identify_calibration_and_science_runs, polynomial_function
@@ -97,7 +99,8 @@ master_flat, noise = extract_orders(
     ccd1_runs = calibration_runs['Flat_60.0'][:1],
     ccd2_runs = calibration_runs['Flat_1.0'][:1],
     ccd3_runs = calibration_runs['Flat_0.1'][:1],
-    Flat = True
+    Flat = True,
+    #debug_overscan=True
 )
 
 # Extract Master ThXe
@@ -130,7 +133,8 @@ for science_object in list(science_runs.keys()):
             ccd1_runs = science_runs[science_object],
             ccd2_runs = science_runs[science_object],
             ccd3_runs = science_runs[science_object],
-            Science=True
+            Science=True,
+            #debug_overscan=True
         )
 
         # Create a primary HDU and HDU list
@@ -144,11 +148,11 @@ for science_object in list(science_runs.keys()):
             # Define the columns with appropriate formats
             col1_def = fits.Column(name='wave_vac',format='E', array=np.arange(len(science[ext_index,:]),dtype=float))
             col2_def = fits.Column(name='wave_air',format='E', array=np.arange(len(science[ext_index,:]),dtype=float))
-            col3_def = fits.Column(name='science', format='E', array=science[ext_index,:])
-            col4_def = fits.Column(name='science_noise',   format='E', array=science_noise[ext_index,:])
+            col3_def = fits.Column(name='science', format='E', array=science[ext_index,:]/master_flat[ext_index,:])
+            col4_def = fits.Column(name='science_noise',   format='E', array=science_noise[ext_index,:]/master_flat[ext_index,:])
             col5_def = fits.Column(name='flat',    format='E', array=master_flat[ext_index,:])
-            col6_def = fits.Column(name='thxe',    format='E', array=master_thxe[ext_index,:])
-            col7_def = fits.Column(name='lc',      format='E', array=master_lc[ext_index,:])
+            col6_def = fits.Column(name='thxe',    format='E', array=master_thxe[ext_index,:]/master_flat[ext_index,:])
+            col7_def = fits.Column(name='lc',      format='E', array=master_lc[ext_index,:]/master_flat[ext_index,:])
 
             hdu = fits.BinTableHDU.from_columns([col1_def, col2_def, col3_def, col4_def, col5_def, col6_def, col7_def], name=ext_name.lower())
 
@@ -156,8 +160,9 @@ for science_object in list(science_runs.keys()):
             hdul.append(hdu)
 
         # Save to a new FITS file with an extension for each order
-        Path(config.working_directory+'reduced_data/'+config.date+'/'+science_object).mkdir(parents=True, exist_ok=True)    
-        hdul.writeto(config.working_directory+'reduced_data/'+config.date+'/'+science_object+'/veloce_spectra_'+science_object+'_'+config.date+'.fits', overwrite=True)
+        Path(config.working_directory+'reduced_data/'+config.date+'/'+science_object).mkdir(parents=True, exist_ok=True)
+        spectrum_filename = 'veloce_spectra_'+science_object+'_'+config.date+'.fits'
+        hdul.writeto(config.working_directory+'reduced_data/'+config.date+'/'+science_object+'/'+spectrum_filename, overwrite=True)
 
         print('Successfully extracted '+science_object)
 