72e30541bf3120691e5aa008c04222a089bff808
svenbuder <sven.buder@anu.edu.au>
2024-12-20 16:15:06 +1100

    including a Voigt Profile fit for Halpha in order 93 and 94 as well as CaT 8542 and 8662 in order 71. These lead to ~3km/s radial velocity accuracy and precision at the moment. Also included vbary correction, but needed to apply negative correction from the one reported by astropy to get accurate final RV estimates. VRAD and E_VRAD are now reported in the FITS header as well (to be used for plotting) and are in fact used when plotting the VRAD and BARYVEL corrected spectra (for comparison with literature spectra). All reversible, because reported.

--- VeloceReduction_tutorial.py
+++ VeloceReduction_tutorial.py
@@ -98,101 +98,92 @@ initial_order_ranges, initial_order_coeffs = VR.extraction.extract_initial_order
 # In[ ]:
 
 
-# Extract Master Flat
-print('Extracting Master Flat')
-master_flat, noise = VR.extraction.extract_orders(
-    ccd1_runs = calibration_runs['Flat_60.0'][:1],
-    ccd2_runs = calibration_runs['Flat_1.0'][:1],
-    ccd3_runs = calibration_runs['Flat_0.1'][:1],
-    Flat = True,
-    #debug_overscan=True
-)
+# # Extract Master Flat
+# print('Extracting Master Flat')
+# master_flat, noise = VR.extraction.extract_orders(
+#     ccd1_runs = calibration_runs['Flat_60.0'][:1],
+#     ccd2_runs = calibration_runs['Flat_1.0'][:1],
+#     ccd3_runs = calibration_runs['Flat_0.1'][:1],
+#     Flat = True,
+#     #debug_overscan=True
+# )
 
-# Extract Master ThXe
-print('Extracting Master ThXe')
-master_thxe, noise = VR.extraction.extract_orders(
-    ccd1_runs = calibration_runs['FibTh_180.0'][:1],
-    ccd2_runs = calibration_runs['FibTh_60.0'][:1],
-    ccd3_runs = calibration_runs['FibTh_15.0'][:1]
-)
+# # Extract Master ThXe
+# print('Extracting Master ThXe')
+# master_thxe, noise = VR.extraction.extract_orders(
+#     ccd1_runs = calibration_runs['FibTh_180.0'][:1],
+#     ccd2_runs = calibration_runs['FibTh_60.0'][:1],
+#     ccd3_runs = calibration_runs['FibTh_15.0'][:1]
+# )
 
-# Extract Master LC
-print('Extracting Master LC')
-master_lc, noise = VR.extraction.extract_orders(
-    ccd1_runs = calibration_runs['SimLC'][-1:],
-    ccd2_runs = calibration_runs['SimLC'][-1:],
-    ccd3_runs = calibration_runs['SimLC'][-1:],
-    LC = True,
-    # tramline_debug = True
-)
+# # Extract Master LC
+# print('Extracting Master LC')
+# master_lc, noise = VR.extraction.extract_orders(
+#     ccd1_runs = calibration_runs['SimLC'][-1:],
+#     ccd2_runs = calibration_runs['SimLC'][-1:],
+#     ccd3_runs = calibration_runs['SimLC'][-1:],
+#     LC = True,
+#     # tramline_debug = True
+# )
 
 
 # In[ ]:
 
 
-# Extract Science Objects and save them into FITS files under reduced_data/
-for science_object in list(science_runs.keys()):
-    print('Extracting '+science_object)
-    try:
-        science, science_noise, science_header = VR.extraction.extract_orders(
-            ccd1_runs = science_runs[science_object],
-            ccd2_runs = science_runs[science_object],
-            ccd3_runs = science_runs[science_object],
-            Science=True,
-            #debug_overscan=True
-        )
+# # Extract Science Objects and save them into FITS files under reduced_data/
+# for science_object in list(science_runs.keys()):
+#     print('Extracting '+science_object)
+#     try:
+#         science, science_noise, science_header = VR.extraction.extract_orders(
+#             ccd1_runs = science_runs[science_object],
+#             ccd2_runs = science_runs[science_object],
+#             ccd3_runs = science_runs[science_object],
+#             Science=True,
+#             #debug_overscan=True
+#         )
 
-        # Create a primary HDU and HDU list
-        primary_hdu = fits.PrimaryHDU()
-        header = primary_hdu.header
-        header['OBJECT']  = (science_header['OBJECT'], 'Name of observed object in night log')
-        header['UTMJD']   = (science_header['UTMJD'],  'Modified Julian Date of observation')
-        header['MEANRA']  = (science_header['MEANRA'], 'Mean Right Ascension of observed object')
-        header['MEANDEC'] = (science_header['MEANDEC'],'Mean Declination of observed object')        
-        header['BARYVEL'] = ('None',                   'Applied barycentric velocity correction')
-        hdul = fits.HDUList([primary_hdu])
+#         # Create a primary HDU and HDU list
+#         primary_hdu = fits.PrimaryHDU()
+#         header = primary_hdu.header
+#         header['OBJECT']  = (science_header['OBJECT'], 'Name of observed object in night log')
+#         header['UTMJD']   = (science_header['UTMJD'],  'Modified Julian Date of observation')
+#         header['MEANRA']  = (science_header['MEANRA'], 'Mean Right Ascension of observed object')
+#         header['MEANDEC'] = (science_header['MEANDEC'],'Mean Declination of observed object')        
+#         header['BARYVEL'] = ('None',                   'Applied barycentric velocity correction')
+#         header['VRAD']    = ('None',                   'Radial velocity estimate')
+#         header['E_VRAD']  = ('None',                   'Uncertainty of radial velocity estimate')
+#         hdul = fits.HDUList([primary_hdu])
 
-        # Loop over your extension names and corresponding data arrays
-        for ext_index, ext_name in enumerate(initial_order_coeffs):
-            # Create an ImageHDU object for each extension
+#         # Loop over your extension names and corresponding data arrays
+#         for ext_index, ext_name in enumerate(initial_order_coeffs):
+#             # Create an ImageHDU object for each extension
 
-            # Define the columns with appropriate formats
-            col1_def = fits.Column(name='wave_vac',format='E', array=np.arange(len(science[ext_index,:]),dtype=float))
-            col2_def = fits.Column(name='wave_air',format='E', array=np.arange(len(science[ext_index,:]),dtype=float))
-            col3_def = fits.Column(name='science', format='E', array=science[ext_index,:]/master_flat[ext_index,:])
-            col4_def = fits.Column(name='science_noise',   format='E', array=science_noise[ext_index,:]/master_flat[ext_index,:])
-            col5_def = fits.Column(name='flat',    format='E', array=master_flat[ext_index,:])
-            col6_def = fits.Column(name='thxe',    format='E', array=master_thxe[ext_index,:]/master_flat[ext_index,:])
-            col7_def = fits.Column(name='lc',      format='E', array=master_lc[ext_index,:]/master_flat[ext_index,:])
+#             # Define the columns with appropriate formats
+#             col1_def = fits.Column(name='wave_vac',format='E', array=np.arange(len(science[ext_index,:]),dtype=float))
+#             col2_def = fits.Column(name='wave_air',format='E', array=np.arange(len(science[ext_index,:]),dtype=float))
+#             col3_def = fits.Column(name='science', format='E', array=science[ext_index,:]/master_flat[ext_index,:])
+#             col4_def = fits.Column(name='science_noise',   format='E', array=science_noise[ext_index,:]/master_flat[ext_index,:])
+#             col5_def = fits.Column(name='flat',    format='E', array=master_flat[ext_index,:])
+#             col6_def = fits.Column(name='thxe',    format='E', array=master_thxe[ext_index,:]/master_flat[ext_index,:])
+#             col7_def = fits.Column(name='lc',      format='E', array=master_lc[ext_index,:]/master_flat[ext_index,:])
 
-            hdu = fits.BinTableHDU.from_columns([col1_def, col2_def, col3_def, col4_def, col5_def, col6_def, col7_def], name=ext_name.lower())
+#             hdu = fits.BinTableHDU.from_columns([col1_def, col2_def, col3_def, col4_def, col5_def, col6_def, col7_def], name=ext_name.lower())
 
-            # Append the HDU to the HDU list
-            hdul.append(hdu)
+#             # Append the HDU to the HDU list
+#             hdul.append(hdu)
 
-        # Save to a new FITS file with an extension for each order
-        Path(config.working_directory+'reduced_data/'+config.date+'/'+science_object).mkdir(parents=True, exist_ok=True)
-        spectrum_filename = 'veloce_spectra_'+science_object+'_'+config.date+'.fits'
-        hdul.writeto(config.working_directory+'reduced_data/'+config.date+'/'+science_object+'/'+spectrum_filename, overwrite=True)
+#         # Save to a new FITS file with an extension for each order
+#         Path(config.working_directory+'reduced_data/'+config.date+'/'+science_object).mkdir(parents=True, exist_ok=True)
+#         spectrum_filename = 'veloce_spectra_'+science_object+'_'+config.date+'.fits'
+#         hdul.writeto(config.working_directory+'reduced_data/'+config.date+'/'+science_object+'/'+spectrum_filename, overwrite=True)
 
-        print('  -> Successfully extracted '+science_object)
+#         print('  -> Successfully extracted '+science_object)
 
-    except:
-        print('  -> Failed to extract '+science_object)
+#     except:
+#         print('  -> Failed to extract '+science_object)
 
 
 # ## Wavelength calibration
-
-# In[ ]:
-
-
-for science_object in list(science_runs.keys()):
-#     try:
-    VR.calibration.calibrate_wavelength(science_object, correct_barycentric_velocity=True, create_overview_pdf=False)
-    print('  -> Succesfully calibrated wavelength without diagnostic plots for '+science_object)
-#     except:
-#         print('  -> Failed to calibrate wavelength for '+science_object)
-
 
 # In[ ]:
 
